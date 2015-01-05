ad59bc41f409cdbc8523e5c4238706356bb07087
Will Foran <willforan@gmail.com>
2015-01-05 10:50:11 -0500

    try RT, try <15; no luck

--- findMedation.R
+++ findMedation.R
@@ -9,29 +9,48 @@
 #  Age --*-- % correct
 #
 #
-#   per.cor ~ part.coeff:  P>|z| == .569
+#  p=.29 for medation 
 ##
 
 
 # install needed packages
 #install.packages(c('MBESS','gsl','lavaan') )
+library(MBESS)  # mediation
+library(lavaan) # sem
 
-# load data
-x<-read.table('agePCperf.csv', sep=",",header=F)
-names(x) <- c('age','part.coeff','per.cor')
+# load age, participation coeff and performance (percent correct)
+allx<-read.table('agePCperf.csv', sep=",",header=F)
+names(allx) <- c('age','part.coeff','per.cor')
+
+# we only really care about development
+maxage<-15
+x <- allx[ allx$age < maxage, ]
 
 # try mediation from MBESS package
-library(MBESS)
-mediation(x$age, x$part.coeff, x$per.cor)
+v<-mediation(x$age, x$part.coeff, x$per.cor)
 
 
 
 # use SEM from lavaan
-library(lavaan)
 m <- '
 per.cor ~ age
 per.cor ~ part.coeff
 part.coeff ~ age
 '
 summary(sem(m, x))
+
+
+
+
+
+####
+# look at RT instead of %corr
+# p is .33 if allx, .44 if unly under 15 yo
+allx<-read.table('RT_age_PC.csv', sep=",",header=F)
+names(allx) <- c('age','part.coeff','RT')
+x <- allx[ allx$age < maxage, ]
+v <- mediation(x$age, x$part.coeff, x$RT)
+p <- v$Y.on.X.and.M$Regression.Table[3,4] 
+print(p) # still not significant
+
 
